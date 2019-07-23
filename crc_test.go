f7695e292343719e322314183d0ae5271ac4d665
Jan Cajthaml <jan.cajthaml@gmail.com>
2019-07-23 18:21:28 +0200

    wip

--- crc_test.go
+++ crc_test.go
@@ -1,12 +1,9 @@
-package main
+package crc32
 
 import (
-	"encoding/binary"
-	"encoding/hex"
 	"hash/crc32"
 	"strings"
 	"testing"
-	"unsafe"
 )
 
 var largeText = []byte(strings.Repeat("a", 50000))
@@ -14,102 +11,64 @@ var smallText = []byte(strings.Repeat("a", 5))
 
 const IEEE = 0xedb88320
 
-func naive(data []byte, polynomial uint32) []byte {
-	var crc uint32 = 0 ^ 0xFFFFFFFF
-
-	for _, current := range data {
-		crc ^= uint32(current)
-		for j := 0; j < 8; j++ {
-			crc = (crc >> 1) ^ (crc&1)*polynomial
-		}
-	}
-
-	return ((*[4]byte)(unsafe.Pointer(&crc)))[:]
+func standard(data []byte) uint32 {
+	return crc32.ChecksumIEEE(data) ^ 0xFFFFFFFF
 }
 
-func standard(data []byte) []byte {
-	result := make([]byte, 4)
-	binary.LittleEndian.PutUint32(result, crc32.ChecksumIEEE(data)^0xFFFFFFFF)
-	return result
-}
-
-func BenchmarkNaiveSmall(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		naive(smallText, IEEE)
+func AssetEqual(t *testing.T, expected uint32, actual uint32) {
+	if expected != actual {
+		t.Errorf("Expected 0x%08X got 0x%08X", expected, actual)
 	}
 }
 
-func BenchmarkNaiveLarge(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		naive(largeText, IEEE)
+func TestCrc32EmptyVector(t *testing.T) {
+	input := []byte{}
+	libResult := Checksum(input, IEEE, 0xFFFFFFFF, 0x00000000)
+	standardResult := standard(input)
+
+	AssetEqual(t, standardResult, libResult)
+}
+
+// https://crccalc.com/
+func TestNormalized(t *testing.T) {
+
+	input := []byte("abcdefgh")
+
+	t.Log("CRC-32/BZIP2")
+	{
+		AssetEqual(t, 0x5024EC61, Checksum(input, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF))
 	}
+
 }
 
 func BenchmarkStandardSmall(b *testing.B) {
+	b.ResetTimer()
+	b.SetBytes(int64(len(smallText)))
 	for n := 0; n < b.N; n++ {
 		standard(smallText)
 	}
 }
 
 func BenchmarkStandardLarge(b *testing.B) {
+	b.ResetTimer()
+	b.SetBytes(int64(len(largeText)))
 	for n := 0; n < b.N; n++ {
 		standard(largeText)
 	}
 }
 
 func BenchmarkCrcSmall(b *testing.B) {
+	b.ResetTimer()
+	b.SetBytes(int64(len(smallText)))
 	for n := 0; n < b.N; n++ {
-		CRC32(smallText, IEEE)
+		Checksum(smallText, IEEE, 0xFFFFFFFF, 0x00000000)
 	}
 }
 
 func BenchmarkCrcLarge(b *testing.B) {
+	b.ResetTimer()
+	b.SetBytes(int64(len(largeText)))
 	for n := 0; n < b.N; n++ {
-		CRC32(largeText, IEEE)
-	}
-}
-
-func TestCrc32EmptyVector(t *testing.T) {
-	input := []byte{}
-	libResult := CRC32(input, IEEE)
-	naiveResult := naive(input, IEEE)
-	standardResult := standard(input)
-
-	a := hex.EncodeToString(libResult)
-	b := hex.EncodeToString(naiveResult)
-	c := hex.EncodeToString(standardResult)
-
-	if a != b || b != c {
-		t.Errorf("Invalid CRC32. standard 0x" + c + " naive 0x" + b + " library 0x" + a)
-	}
-}
-
-func TestCrc32Vector1(t *testing.T) {
-	input := []byte("a")
-	libResult := CRC32(input, IEEE)
-	naiveResult := naive(input, IEEE)
-	standardResult := standard(input)
-
-	a := hex.EncodeToString(libResult)
-	b := hex.EncodeToString(naiveResult)
-	c := hex.EncodeToString(standardResult)
-
-	if a != b || b != c {
-		t.Errorf("Invalid CRC32. standard 0x" + c + " naive 0x" + b + " library 0x" + a)
-	}
-}
-
-func TestCrc32Vector2(t *testing.T) {
-	input := []byte(largeText)
-	libResult := CRC32(input, IEEE)
-	naiveResult := naive(input, IEEE)
-	standardResult := standard(input)
-
-	a := hex.EncodeToString(libResult)
-	b := hex.EncodeToString(naiveResult)
-	c := hex.EncodeToString(standardResult)
-
-	if a != b || b != c {
-		t.Errorf("Invalid CRC32. standard 0x" + c + " naive 0x" + b + " library 0x" + a)
+		Checksum(largeText, IEEE, 0xFFFFFFFF, 0x00000000)
 	}
 }
